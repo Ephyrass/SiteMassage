c292ca89386a62ae2b48a80cbbe195b164ff1649
ephyrass <jb.houi@yahoo.fr>
2021-06-25 12:18:05 +0200

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '49b98593cacb3afea6981583c47c5262'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '23ddadd1-93d8-405c-802f-e559051ffb34'
+};
